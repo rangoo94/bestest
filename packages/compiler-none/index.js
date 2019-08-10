1d24f3f41086343c1d6be3cc211bee110391b745
Dawid Rusnak <dawid@drcode.pl>
2019-08-10 09:06:28 +0200

    Fix .gitignores & remove auto-generated files

--- packages/compiler-none/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DummyCompilerAdapter_1 = require("./lib/DummyCompilerAdapter");
-exports["default"] = DummyCompilerAdapter_1.DummyCompilerAdapter;
--- packages/compiler-webpack/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var WebpackCompilerAdapter_1 = require("./lib/WebpackCompilerAdapter");
-exports["default"] = WebpackCompilerAdapter_1.WebpackCompilerAdapter;
